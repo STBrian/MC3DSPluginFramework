498778fac91ca63873e62f71b25577f325e561e5
STBrian <brichap100@gmail.com>
2026-06-02 23:51:31 -0600

    Compilation fixes

--- Sources/Helpers/GradRender.cpp
+++ Sources/Helpers/GradRender.cpp
@@ -20,110 +20,110 @@ namespace GradRender
     constexpr float MINBRIGHTNESS = 0.f;
     constexpr float SHADOW_POWER  = 0.1f;
 
-    int drawGlyph(const CTRPF::Screen &scr, const CTRPF::Glyph *glyph, int posX, const int posY, const CTRPF::Color &baseColor, const bool shadow)
-    {
-        if (!glyph)
-            return posX;
+    // int drawGlyph(const CTRPF::Screen &scr, const CTRPF::Glyph *glyph, int posX, const int posY, const CTRPF::Color &baseColor, const bool shadow)
+    // {
+    //     if (!glyph)
+    //         return posX;
 
-        posX += glyph->xOffset;
-        const u8 *data                   = glyph->glyph;
-        static constexpr int glyphHeight = 16;
+    //     posX += glyph->xOffset;
+    //     const u8 *data                   = glyph->glyph;
+    //     static constexpr int glyphHeight = 16;
 
-        const auto Blend_Draw =
-            [&](int x, int y, const CTRPF::Color &input) {
-                CTRPF::Color c;
-                scr.ReadPixel(x, y, c);
-                scr.DrawPixel(x, y, c.Blend(input, CTRPF::Color::BlendMode::Alpha));
-            };
+    //     const auto Blend_Draw =
+    //         [&](int x, int y, const CTRPF::Color &input) {
+    //             CTRPF::Color c;
+    //             scr.ReadPixel(x, y, c);
+    //             scr.DrawPixel(x, y, c.Blend(input, CTRPF::Color::BlendMode::Alpha));
+    //         };
 
-        for (int row = 0; row < glyphHeight; ++row) {
-            float brightness = MAXBRIGHTNESS - ((float)row / (float)glyphHeight) * (MAXBRIGHTNESS - MINBRIGHTNESS);
-            brightness       = std::clamp(brightness, 0.0f, 2.0f);
+    //     for (int row = 0; row < glyphHeight; ++row) {
+    //         float brightness = MAXBRIGHTNESS - ((float)row / (float)glyphHeight) * (MAXBRIGHTNESS - MINBRIGHTNESS);
+    //         brightness       = std::clamp(brightness, 0.0f, 2.0f);
 
-            u8 r = (u8)(std::min(baseColor.r * brightness, 255.0f));
-            u8 g = (u8)(std::min(baseColor.g * brightness, 255.0f));
-            u8 b = (u8)(std::min(baseColor.b * brightness, 255.0f));
-            CTRPF::Color gradColor(r, g, b);
+    //         u8 r = (u8)(std::min(baseColor.r * brightness, 255.0f));
+    //         u8 g = (u8)(std::min(baseColor.g * brightness, 255.0f));
+    //         u8 b = (u8)(std::min(baseColor.b * brightness, 255.0f));
+    //         CTRPF::Color gradColor(r, g, b);
 
-            for (int col = 0; col < 13; ++col) {
-                u8 alpha = data[row * 13 + col];
+    //         for (int col = 0; col < 13; ++col) {
+    //             u8 alpha = data[row * 13 + col];
 
-                if (alpha <= 12)
-                    continue;
+    //             if (alpha <= 12)
+    //                 continue;
 
-                int drawX   = posX + col;
-                int drawY   = posY + row;
-                gradColor.a = alpha;
+    //             int drawX   = posX + col;
+    //             int drawY   = posY + row;
+    //             gradColor.a = alpha;
 
-                if (shadow) {
-                    CTRPF::Color shadowColor((u8)(r * SHADOW_POWER), (u8)(g * SHADOW_POWER), (u8)(b * SHADOW_POWER), alpha);
-                    Blend_Draw(drawX + 1, drawY + 1, shadowColor);
-                }
+    //             if (shadow) {
+    //                 CTRPF::Color shadowColor((u8)(r * SHADOW_POWER), (u8)(g * SHADOW_POWER), (u8)(b * SHADOW_POWER), alpha);
+    //                 Blend_Draw(drawX + 1, drawY + 1, shadowColor);
+    //             }
 
-                Blend_Draw(drawX, drawY, gradColor);
-            }
-        }
-        return posX + glyph->xAdvance;
-    }
+    //             Blend_Draw(drawX, drawY, gradColor);
+    //         }
+    //     }
+    //     return posX + glyph->xAdvance;
+    // }
 
-    int drawSysfont(const CTRPF::Screen &scr, const std::string &text, const int posX, const int posY, const bool shadow, int xLimits, float offset, const char *end)
-    {
-        CTRPF::Color color = CTRPF::Color::White;
-        int x              = posX;
-        int y              = posY;
-        u8 *str            = (u8 *)text.c_str();
+    // int drawSysfont(const CTRPF::Screen &scr, const std::string &text, const int posX, const int posY, const bool shadow, int xLimits, float offset, const char *end)
+    // {
+    //     CTRPF::Color color = CTRPF::Color::White;
+    //     int x              = posX;
+    //     int y              = posY;
+    //     u8 *str            = (u8 *)text.c_str();
 
-        if (!(str && *str))
-            return x;
+    //     if (!(str && *str))
+    //         return x;
 
-        xLimits = std::min(xLimits, (scr.IsTop ? 400 : 320));
+    //     xLimits = std::min(xLimits, (scr.IsTop ? 400 : 320));
 
-        if (str[0] == 0xEF && str[1] == 0xBB && str[2] == 0xBF)
-            str += 3;
+    //     if (str[0] == 0xEF && str[1] == 0xBB && str[2] == 0xBF)
+    //         str += 3;
 
-        do {
-            if (x >= xLimits || str == (u8 *)end)
-                break;
+    //     do {
+    //         if (x >= xLimits || str == (u8 *)end)
+    //             break;
 
-            u8 c = *str;
+    //         u8 c = *str;
 
-            if (c == '\n' || c == '\r') {
-                ++str;
-                x = posX;
-                y += 16;
-                continue;
-            }
+    //         if (c == '\n' || c == '\r') {
+    //             ++str;
+    //             x = posX;
+    //             y += 16;
+    //             continue;
+    //         }
 
-            if (c == 0x1B) {
-                ++str;
-                color.r = *str++;
-                color.g = *str++;
-                color.b = *str++;
-                continue;
-            }
+    //         if (c == 0x1B) {
+    //             ++str;
+    //             color.r = *str++;
+    //             color.g = *str++;
+    //             color.b = *str++;
+    //             continue;
+    //         }
 
-            CTRPF::Glyph *glyph = CTRPluginFramework::Font::GetGlyph(str);
+    //         CTRPF::Glyph *glyph = CTRPluginFramework::Font::GetGlyph(str);
 
-            if (!glyph)
-                break;
+    //         if (!glyph)
+    //             break;
 
-            float gWidth = glyph->Width();
-            if (x + gWidth >= xLimits) {
-                x = posX;
-                y += 16;
-            }
+    //         float gWidth = glyph->Width();
+    //         if (x + gWidth >= xLimits) {
+    //             x = posX;
+    //             y += 16;
+    //         }
 
-            if (offset >= gWidth) {
-                offset -= gWidth;
-                continue;
-            }
+    //         if (offset >= gWidth) {
+    //             offset -= gWidth;
+    //             continue;
+    //         }
 
-            x = drawGlyph(scr, glyph, x, y, color, shadow);
+    //         x = drawGlyph(scr, glyph, x, y, color, shadow);
 
-        } while (*str);
+    //     } while (*str);
 
-        return y + 16;
-    }
+    //     return y + 16;
+    // }
 
     bool main(const CTRPF::Screen &scr)
     {
--- Sources/Helpers/GradRender.hpp
+++ Sources/Helpers/GradRender.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <CTRPluginFramework.hpp>
-#include <CTRPluginFrameworkImpl.hpp>
+// #include <CTRPluginFrameworkImpl.hpp>
 
 namespace GradRender
 {
     namespace CTRPF = CTRPluginFramework;
 
-    int  drawGlyph(const CTRPF::Screen &scr, const CTRPF::Glyph *glyph, int posX, const int posY, const CTRPF::Color &baseColor, const bool shadow);
+    // int  drawGlyph(const CTRPF::Screen &scr, const CTRPF::Glyph *glyph, int posX, const int posY, const CTRPF::Color &baseColor, const bool shadow);
     int  drawSysfont(const CTRPF::Screen &scr, const std::string &text, const int posX, const int posY, const bool shadow, int xLimits = 400, float offset = 0, const char *end = nullptr);
     void install();
     void unInstall();
